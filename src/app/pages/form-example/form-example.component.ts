bcc6dd30f0f828807f4c2d17565cb32c7752b936
Alexander Lehner <xander_lehner@gmx.at>
2021-04-27 17:46:46 +0200

    add custom styled select

--- src/app/pages/form-example/form-example.component.ts
+++ src/app/pages/form-example/form-example.component.ts
@@ -14,6 +14,7 @@ export class FormExampleComponent {
 
     public checkedTestingTools = '';
     public email = '';
+    public gender = '';
     public favoriteScreenReader = '';
     public fullName = '';
     public isSuccessfullySubmitted = false;
