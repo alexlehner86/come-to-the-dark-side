d6deea879f8869e9408c5d82dae23fbeefcf34b3
Alexander Lehner <xander_lehner@gmx.at>
2022-04-07 22:07:21 +0200

    translate home page to German

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,8 +1,31 @@
-import { Component } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+    selector: 'app-home',
+    templateUrl: './home.component.html',
+    styleUrls: ['./home.component.scss']
 })
-export class HomeComponent { }
+export class HomeComponent implements OnInit, OnDestroy {
+
+    public isGerman = false;
+
+    private subscriptions: Subscription[] = [];
+
+    constructor(
+        private translateService: TranslateService
+    ) { }
+
+    public ngOnInit(): void {
+        this.subscriptions.push(
+            this.translateService.onLangChange.subscribe(({ lang }) => {
+                this.isGerman = lang === 'de';
+            })
+        );
+    }
+
+    public ngOnDestroy(): void {
+        this.subscriptions.forEach(sub => sub.unsubscribe());
+    }
+}
